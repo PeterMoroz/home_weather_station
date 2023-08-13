6c86b9c042bb70bdd4788608b058090c8155e4ad
Peter Moroz <ptr.moroz@gmail.com>
2023-08-13 21:11:06 +0300

    add: 1. description, schematic. 2. example sketch to read temperature and humidity.

---
+++ examples/dht22/weather_station.ino
@@ -0,0 +1,29 @@
+#include "DHTesp.h"
+
+DHTesp dht;
+
+int minSamplingPeriodMs = 0x7FFFFFFF;
+long lastTimeReadSensor = 0;
+
+// the type 'TempAndHumidity' defined in DHTesp library
+TempAndHumidity th = { 0.f, 0.f };
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // the sensor connected to D0 pin of Wemos D1 board
+  // the sensor's model is DHT22
+  dht.setup(D0, DHTesp::DHT22);
+
+  minSamplingPeriodMs = dht.getMinimumSamplingPeriod();
+}
+
+void loop() {
+  long currTime = millis();
+  if ((currTime - lastTimeReadSensor) < minSamplingPeriodMs) {
+    lastTimeReadSensor = currTime;
+    th = dht.getTempAndHumidity();
+    Serial.printf("temperature: %.2f celsius degrees, humidity: %.2f %%\n", th.temperature, th.humidity);
+  }
+}
